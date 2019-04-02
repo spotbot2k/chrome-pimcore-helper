d70ad57cb753532fc2094a885a81c374d0ee1b61
Alexander Schwirjow <alexander@schwirjow.de>
2019-04-02 21:14:38 +0200

    Extend the sceleton, add first lines of code

---
+++ src/app.js
@@ -0,0 +1,9 @@
+let app = {
+    init: function() {
+        document.querySelector('#js-app-apply').addEventListener('click', this.apply, false);
+    },
+    apply: function() {
+        alert('not implemented yet');
+    }
+}
+app.init();
